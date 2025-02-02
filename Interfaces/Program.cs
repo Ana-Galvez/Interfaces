040fb2ba43b5f9f37347e2d3b626fa61613032fc
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2025-02-02 01:29:08 -0300

    feat: clases

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -8,4 +8,52 @@ namespace Interfaces
         {
         }
     }
+
+    class Mamiferos
+    {
+        public Mamiferos(String nombreMamifero)
+        {
+            Console.WriteLine("Mi nombre es " + nombreMamifero);
+        }
+    }
+
+    class Ballena : Mamiferos
+    {
+        public Ballena(String nombreBallena) : base(nombreBallena)
+        {
+
+        }
+
+        public void Nadar()
+        {
+            Console.WriteLine("La ballena puede nadar");
+        }
+    }
+
+    class Humano : Mamiferos
+    {
+        public Humano(String nombreHumano) : base(nombreHumano)
+        {
+          
+        }
+
+        public void Pensar()
+        {
+            Console.WriteLine("El humano puede pensar");
+        }
+    }
+
+    class Caballo : Mamiferos
+    {
+        public Caballo(String nombreCaballo) : base(nombreCaballo)
+        {
+
+        }
+
+        public void Galopar()
+        {
+            Console.WriteLine("El caballo puede galopar");
+        }
+
+    }
 }
